36d14fc6740a40da1f3df9755995aad7f7669b73
Sebastian014 <1test94@web.de>
2017-01-14 12:31:56 +0100

    Stringeingabe erzeugt

---
+++ 02_Jupyter/lib/Input_Paser.py
@@ -0,0 +1,47 @@
+
+# coding: utf-8
+
+# In[34]:
+
+from IPython.display import display
+from IPython.display import clear_output
+
+from ipywidgets import Accordion
+from ipywidgets import Layout
+from ipywidgets import Text
+
+
+# In[33]:
+
+class Class_Input_Paser(object):
+    def __init__(self):
+        self.RS_Titel = 'Funktionseingabe'
+       
+        self.F_wdg_Layout = Layout(display='flex',flex_flow='row',justify_content='center')
+        self.F_wdg_Box_Layout = Layout(display='flex',flex_flow='column',align_items='stretch',width='100%')
+        
+        self.Ftxt_Input = Text(layout=self.F_wdg_Layout,visible = True) 
+        self.FInput = None
+        self.FAccItems = [self.Ftxt_Input]
+        self.FAcc =  Accordion(children=self.FAccItems) 
+        
+        
+        self.create()
+    
+    
+    def txt_Event_Eingabe (self,ADummy):
+        self.FInput = self.Ftxt_Input.value       
+        
+        
+    
+    def create(self):
+        self.Ftxt_Input.observe(self.txt_Event_Eingabe)
+        
+        self.FAcc.set_title(0, self.RS_Titel)
+        display(self.FAcc)  
+
+
+# In[ ]:
+
+
+
